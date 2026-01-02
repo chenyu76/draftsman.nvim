d00e0d25b65ba81ac281570e9aedbe5749550cd8
chenyu76 <51879608+chenyu76@users.noreply.github.com>
2026-01-02 10:38:32 +0800

    change keys

--- lua/draftsman/config.lua
+++ lua/draftsman/config.lua
@@ -21,7 +21,6 @@ M.defaults = {
 			[[<-> ]],
 		},
 	},
-	commit_key = "<CR>",
 	integrations = {
 		-- disable some builtin mini.nvim plugins to
 		-- prevent conflicts
--- lua/draftsman/keys.lua
+++ lua/draftsman/keys.lua
@@ -42,20 +42,35 @@ function M.set_mappings(stop_callback)
 	-- Tools
 	map_hybrid("?", ui.toggle_help)
 	map_hybrid("e", function()
-		state.mode, state.last_dir = "edge", nil
-		ui.update_status("Edge Mode.\n<Space> to commit.")
+		if state.mode == "edge" then
+			state.mode = nil
+			ui.update_status("Ended Edge Draw.")
+		else
+			state.mode, state.last_dir = "edge", nil
+			ui.update_status("Edge Mode.\n<e> to commit.")
+		end
 	end)
 	map_hybrid("a", function()
-		state.mode, state.last_dir = "arrow", nil
-		ui.update_status("Arrow Mode.\n<Space> to commit.")
+		if state.mode == "arrow" then
+			state.mode = nil
+			ui.update_status("Ended Arrow Draw.")
+		else
+			state.mode, state.last_dir = "arrow", nil
+			ui.update_status("Arrow Mode.\n<a> to commit.")
+		end
 	end)
 	map_hybrid("m", function()
-		state.mode = "move"
-		ui.update_status("Move Mode.\n<Space> to commit.")
+		if state.mode == "move" then
+			state.mode = nil
+			ui.update_status("Ended Move Mode.")
+		else
+			state.mode = "move"
+			ui.update_status("Move Mode.\n<m> to commit.")
+		end
 	end)
 	map_hybrid("i", function()
 		state.mode = "text"
-		ui.update_status("Text Input.\n<Space> to commit.")
+		ui.update_status("Text Input.\n<Esc> to commit.")
 	end)
 	map_hybrid("x", function()
 		canvas.set_char_at_cursor(" ")
@@ -69,7 +84,7 @@ function M.set_mappings(stop_callback)
 			state.mode = "box"
 			state.box_start = { canvas.get_virt_row(), canvas.get_virt_col() }
 			ui.update_start_marker()
-			ui.update_status("Box Draw. \n<Space> to commit.")
+			ui.update_status("Box Draw. \n<b> to commit.")
 		end
 	end)
 
@@ -77,7 +92,7 @@ function M.set_mappings(stop_callback)
 		state.mode = "select"
 		state.box_start = { canvas.get_virt_row(), canvas.get_virt_col() }
 		ui.update_start_marker()
-		ui.update_status("Select. \n<d> to delete. \n<y> to yank. \n<Space> to cancel.")
+		ui.update_status("Select. \n<d> to delete. \n<y> to yank. \n<Esc> to cancel.")
 	end)
 
 	-- Clipboard
@@ -85,14 +100,14 @@ function M.set_mappings(stop_callback)
 		if state.mode == "select" then
 			actions.cut_selection()
 		else
-			ui.update_status("Nothing to delete.\nUse <v> first")
+			ui.update_status("Nothing to delete.\nUse <v> first.")
 		end
 	end)
 	map_hybrid("y", function()
 		if state.mode == "select" then
 			actions.copy_selection()
 		else
-			ui.update_status("Nothing to yank.\nUse <v> first")
+			ui.update_status("Nothing to yank.\nUse <v> first.")
 		end
 	end)
 	map_hybrid("p", actions.paste_clipboard)
@@ -135,21 +150,25 @@ function M.set_mappings(stop_callback)
 		end)
 	end
 
-	-- Exit/Commit
+	-- space in text
 	safe_map("<Space>", function()
-		if state.mode == "box" then
-			actions.draw_box_commit()
-		elseif state.mode == "select" then
-			state.mode, state.box_start = nil, nil
-			ui.update_start_marker()
-			ui.update_status("Cancelled")
-		else
-			state.mode = nil
-			ui.update_status("Ready")
+		if state.mode == "text" then
+			canvas.set_char_at_cursor(" ")
+			vim.cmd("normal! l")
 		end
 	end)
 
-	safe_map("<Esc>", stop_callback)
+	-- Stop text, visual and box mode if any
+	-- Otherwise, exit.
+	safe_map("<Esc>", function()
+		if state.mode == "text" or state.mode == "select" or state.mode == "box" then
+			state.mode, state.box_start = nil, nil
+			ui.update_start_marker()
+			ui.update_status("Ready.")
+		else
+			stop_callback()
+		end
+	end)
 
 	-- Text Input Fallback
 	local chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,:;!?-+*/=()[]{}_\"'<>`~@#$%^&"
